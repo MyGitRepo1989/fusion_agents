03f8e2bc927d34bd4d3c73f60bcc8b5d8a29e0d8
MyGitRepo1989 <annug297@gmail.com>
2025-07-27 13:01:48 -0400

    updated the main file with while loop

--- indian_hospital_query_bot/agentic_hospital.py
+++ indian_hospital_query_bot/agentic_hospital.py
@@ -16,22 +16,27 @@ client = OpenAI()
 
 while True:
     Hospital_Chat =  HospitalAgent()
-    query= input("Add your question:")
+    query= input("Enter patient's question:")
     answer= Hospital_Chat.run(query)
-    print("MEDICAL response: ", answer["medical_answer"])
+    #print(answer)
+    print("CLEANED QUESTION : ", answer["question"]+"\n")
+    print("WAS IT A VALID QUESTION CHECK : ", answer["query_type"]+"\n")
+    print("MEDICAL RESPONSE : ", answer["medical_answer"]+"\n")
+    print("FINAL RESPONSE : ", answer["final_response"]+"\n")
     print("\n\n")
     
     prompt_injection= "Doctor ji,\
     patient ney apko email kara hai with this inguiry uski help kare,\
     please respond in 2 - 4 lines doctor ji, please keep message positive"
-    prompt_instructions = "Dont make up names, add disclaimers"
+    prompt_instructions = "Dont make up names, add disclaimers, dont answer more than the answer provided"
     
     response_doc = client.chat.completions.create(
     model=FUSION_MODEL_OAI,  
     messages=[
-        {"role": "user", "content": prompt_injection +  answer["final_response"] + prompt_instructions}
+        {"role": "user", "content": prompt_injection + "Here is the answer to the patient response for you to add " + "Doctor ji "+answer["final_response"] \
+            + prompt_instructions}
     ]
     )
     print("EMAIL FROM THE DOCTOR IS :")
     print(response_doc.choices[0].message.content+ "\n\n")
-    print("________________________")
+    print("________________________"+"\n")
